93b5f194787de1593c43cb518f4be5acb1778439
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-13 12:56:13 +0530

    added: displaying start message

---
+++ stackandqueue/Runner.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.stackandqueue;
+
+public class Runner {
+
+	public static void main(String[] args) {
+	System.out.println("Implementation of Stack and Queue using Generics");
+	}
+
+}
