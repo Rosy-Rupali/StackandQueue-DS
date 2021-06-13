dbdad70da88580f0803ced5ab6811f40c96b97f2
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-13 17:34:08 +0530

    added: Stack implementation

--- stackandqueue/Runner.java
+++ stackandqueue/Runner.java
@@ -3,10 +3,19 @@ package com.bridgelabz.stackandqueue;
 public class Runner {
 
 	public static void main(String[] args) {
-		Stack<Integer> stack = new Stack<Integer>();
+		Stack stack = new Stack();
 		stack.push(70);
 		stack.push(30);
 		stack.push(56);
+		stack.show();
+		System.out.println(stack.peek()+ "peek");
+		System.out.println("\n"+stack.pop()+"pop");
+		System.out.println(stack.peek()+"peek");
+		System.out.println("\n"+stack.pop()+"pop");
+		System.out.println(stack.peek()+"peek");
+		System.out.println("\n"+stack.pop()+"pop");
+		
+		stack.pop();
 		stack.show();
 	}
 
